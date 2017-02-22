cd6126f989294bd6feff79bc5c8951907097cb84
Matthew Wheatley <mwheatley@diamondfortress.com>
2017-02-22 08:32:26 -0600

    added WSQ Image Type
    * added ability to return multiple images

--- src/android/OnyxActivity.java
+++ src/android/OnyxActivity.java
@@ -11,21 +11,26 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.dft.onyx.FingerprintTemplate;
+import com.dft.onyx.NfiqMetrics;
 import com.dft.onyx.core;
 import com.dft.onyx.enroll.util.Consts;
 import com.dft.onyx.enroll.util.EnrolledFingerprintDetails;
 import com.dft.onyx.enroll.util.EnrollmentMetric;
+import com.dft.onyx.enroll.util.RestoreBitmap;
 import com.dft.onyx.fingerwizard.FingerWizardIntentHelper;
 import com.dft.onyx.verify.VerifyIntentHelper;
 import com.dft.onyx.wizardroid.enrollwizard.EnrollWizardBuilder;
 import com.dft.onyxcamera.ui.CaptureConfiguration;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -41,9 +46,12 @@ public class OnyxActivity extends Activity {
     private static final String ONYX_IMAGE_TYPE_RAW = "raw";
     private static final String ONYX_IMAGE_TYPE_PREPROCESSED = "preprocessed";
     private static final String ONYX_IMAGE_TYPE_ENHANCED = "enhanced";
+    private static final String ONYX_IMAGE_TYPE_WSQ = "wsq";
     private Context mContext;
-    private File mFingerprintImageFile;
-
+    private File mRawFingerprintImageFile;
+    private File mPreprocessedFingerprintImageFile;
+    private File mEnhancedFingerprintImageFile;
+    private JSONObject mImageTypes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,79 +65,76 @@ public class OnyxActivity extends Activity {
         String argString = getIntent().getStringExtra("options");
         JSONObject arg_object = null;
         String onyxLicense = null;
-        String action = null;
-        String imageTypeParam = null;
         try {
             arg_object = new JSONObject(argString);
             onyxLicense = arg_object.getString("onyxLicense");
-            action = arg_object.getString("action");
-            imageTypeParam = arg_object.getString("imageType");
+
+            switch (OnyxPlugin.mPluginAction) {
+                case IMAGE:
+                    JSONArray imageTypes = arg_object.getJSONArray("imageTypes");
+                    mImageTypes = imageTypes.toJSONObject(imageTypes);
+
+                    // Always return at least the preprocessed image
+                    if (null == imageTypes ||
+                            imageTypes.length() == 0 ||
+                            mImageTypes.has(ONYX_IMAGE_TYPE_PREPROCESSED) ||
+                            mImageTypes.has(ONYX_IMAGE_TYPE_WSQ)) {
+                        mPreprocessedFingerprintImageFile = new File(
+                                Environment.getExternalStorageDirectory(), "PreprocessedFingerprintImage.jpg");
+                    }
+
+                    if (mImageTypes.has(ONYX_IMAGE_TYPE_RAW)) {
+                        mRawFingerprintImageFile = new File(
+                                Environment.getExternalStorageDirectory(), "RawFingerprintImage.jpg");
+                    }
+                    if (mImageTypes.has(ONYX_IMAGE_TYPE_ENHANCED)) {
+                        mEnhancedFingerprintImageFile = new File(
+                                Environment.getExternalStorageDirectory(), "EnhancedFingerprintImage.jpg");
+                    }
+
+                    Intent fingerImageIntent = FingerWizardIntentHelper.getFingerWizardIntent(
+                            mContext, onyxLicense, false, false, false, CaptureConfiguration.Flip.NONE,
+                            mRawFingerprintImageFile, mEnhancedFingerprintImageFile,
+                            mPreprocessedFingerprintImageFile, Bitmap.CompressFormat.JPEG);
+                    startActivityForResult(fingerImageIntent, RC_ONYX_IMAGE);
+                    break;
+                case ENROLL:
+                    EnrollWizardBuilder ewb = new EnrollWizardBuilder();
+                    ewb.setLicenseKey(onyxLicense);
+                    ewb.setUseSelfEnroll(true);
+                    ewb.setUseOnyxGuide(true, true, false);
+                    ewb.setNumEnrollCapturesPerScale(1);
+                    ewb.setNumEnrollScales(1);
+                    ewb.setMinReticleScale(1.0f);
+                    Intent enrollWizardIntent = ewb.build(mContext);
+                    startActivityForResult(enrollWizardIntent, RC_ONYX_ENROLL);
+                    break;
+                case TEMPLATE:
+                    Intent fingerprintTemplateIntent = FingerWizardIntentHelper.getFingerWizardIntent(
+                            mContext, onyxLicense, false, false, false, CaptureConfiguration.Flip.NONE,
+                            null, null, null, null);
+                    startActivityForResult(fingerprintTemplateIntent, RC_ONYX_TEMPLATE);
+                    break;
+                case VERIFY:
+                    if (fingerprintExists()) {
+                        Intent verifyIntent = VerifyIntentHelper.getVerifyActivityIntent(
+                                mContext, onyxLicense);
+                        startActivityForResult(verifyIntent, RC_ONYX_VERIFY);
+                    } else {
+                        Toast.makeText(mContext, "No fingerprints enrolled.",
+                                Toast.LENGTH_LONG).show();
+                        String errorMessage = "No fingerprints enrolled";
+                        OnyxPlugin.onError(errorMessage);
+                        finish();
+                    }
+                    break;
+            }
         } catch (JSONException e) {
-            String errorMessage = "Error parsing argumets: " + e.toString();
+            String errorMessage = "Error parsing JSON Object: " + e.toString();
             Log.e(TAG, errorMessage);
             OnyxPlugin.onError(errorMessage);
+            finish();
         }
-
-        if (action.equals("image")) {
-            String imageType = "preprocessed";
-            if (imageTypeParam != null) {
-                imageType = imageTypeParam;
-            }
-            // Create a file to hold the fingerprint image bitmap
-            mFingerprintImageFile = new File(Environment.getExternalStorageDirectory(),
-                    "FingerImage.jpg");
-            File rawImageFile = null;
-            File preprocessedImageFile = null;
-            File enhancedImageFile = null;
-
-            if (imageType.equalsIgnoreCase(ONYX_IMAGE_TYPE_RAW)) {
-                rawImageFile = mFingerprintImageFile;
-            } else if (imageType.equalsIgnoreCase(ONYX_IMAGE_TYPE_PREPROCESSED)) {
-                preprocessedImageFile = mFingerprintImageFile;
-            } else if (imageType.equalsIgnoreCase(ONYX_IMAGE_TYPE_ENHANCED)) {
-                enhancedImageFile = mFingerprintImageFile;
-            }
-
-            Intent fingerImageIntent = FingerWizardIntentHelper.getFingerWizardIntent(
-                    mContext, onyxLicense, false, false, CaptureConfiguration.Flip.NONE,
-                    rawImageFile, enhancedImageFile, preprocessedImageFile, Bitmap.CompressFormat.JPEG);
-            startActivityForResult(fingerImageIntent, RC_ONYX_IMAGE);
-        } else if (action.equals("enroll")) {
-            EnrollWizardBuilder ewb = new EnrollWizardBuilder();
-            ewb.setLicenseKey(onyxLicense);
-            ewb.setUseSelfEnroll(true);
-            ewb.setUseOnyxGuide(true, true, false);
-            ewb.setNumEnrollCapturesPerScale(1);
-            ewb.setNumEnrollScales(1);
-            Intent enrollWizardIntent = ewb.build(mContext);
-//            Intent enrollWizardIntent = FingerWizardIntentHelper.getFingerWizardIntent(
-//                    mContext, onyxLicense, false, false, CaptureConfiguration.Flip.NONE,
-//                    null, null, null, null);
-            startActivityForResult(enrollWizardIntent, RC_ONYX_ENROLL);
-        } else if (action.equals("template")) {
-            Intent fingerImageIntent = FingerWizardIntentHelper.getFingerWizardIntent(
-                    mContext, onyxLicense, false, false, CaptureConfiguration.Flip.NONE,
-                    null, null, null, null);
-            startActivityForResult(fingerImageIntent, RC_ONYX_TEMPLATE);
-        } else if (action.equals("verify")) {
-            if (fingerprintExists()) {
-                Intent verifyIntent = VerifyIntentHelper.getVerifyActivityIntent(
-                        mContext, onyxLicense);
-                startActivityForResult(verifyIntent, RC_ONYX_VERIFY);
-            } else {
-                Toast.makeText(mContext, "No fingerprints enrolled.",
-                        Toast.LENGTH_LONG).show();
-                JSONObject result = new JSONObject();
-                try {
-                    result.put("message", "No fingerprints enrolled.");
-                } catch (JSONException e) {
-                    String errorMessage = "Failed to set JSON key value pair: " + e.toString();
-                    Log.e(TAG, errorMessage);
-                    OnyxPlugin.onError(errorMessage);
-                }
-            }
-        }
-
     }
 
     @Override
@@ -139,17 +144,36 @@ public class OnyxActivity extends Activity {
         String errorMessage = null;
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == RC_ONYX_IMAGE) {
-                byte[] bytes = fileToArrayOfBytes(mFingerprintImageFile);
-                if (null != bytes) {
-                    String encodedBytes = Base64.encodeToString(bytes, 0);
-                    String imageUri = "data:image/jpeg;base64," + encodedBytes;
-                    try {
-                        result.put("imageUri", imageUri);
-                    } catch (JSONException e) {
-                        errorMessage = "Failed to set JSON key value pair: " + e.toString();
+                String preprocessedImageUri = null;
+                String rawImageUri = null;
+                String enhancedImageUri = null;
+                JSONObject wsqImageJson = null;
+
+                if (null != mPreprocessedFingerprintImageFile &&
+                        mImageTypes.has(ONYX_IMAGE_TYPE_PREPROCESSED)) {
+                    preprocessedImageUri = createImageUriFromFile(mPreprocessedFingerprintImageFile);
+                }
+                if (null != mRawFingerprintImageFile) {
+                    rawImageUri = createImageUriFromFile(mRawFingerprintImageFile);
+                }
+                if (null != mEnhancedFingerprintImageFile) {
+                    enhancedImageUri = createImageUriFromFile(mEnhancedFingerprintImageFile);
+                }
+
+                JSONObject images = new JSONObject();
+                try {
+
+                    if (mImageTypes.has(ONYX_IMAGE_TYPE_WSQ)) {
+                        wsqImageJson = getWsqImageJson();
                     }
-                } else {
-                    errorMessage = "Error getting fingerprint image bytes.";
+
+                    images.put(ONYX_IMAGE_TYPE_PREPROCESSED, preprocessedImageUri);
+                    images.put(ONYX_IMAGE_TYPE_RAW, rawImageUri);
+                    images.put(ONYX_IMAGE_TYPE_ENHANCED, enhancedImageUri);
+                    images.put(ONYX_IMAGE_TYPE_WSQ, wsqImageJson);
+                    result.put("images", images);
+                } catch (JSONException e) {
+                    errorMessage = "Failed to set JSON key value pair: " + e.toString();
                 }
             } else if (requestCode == RC_ONYX_ENROLL || requestCode == RC_ONYX_TEMPLATE) {
                 // Get the EnrollmentMetric
@@ -181,7 +205,8 @@ public class OnyxActivity extends Activity {
                     }
 
                     if (null != bestTemplate) {
-                        if (bestTemplate.getNfiqScore() < 4) {
+                        Log.d(TAG, "nfiqScore: " + bestTemplate.getNfiqScore());
+                        if (bestTemplate.getNfiqScore() < 5) {
                             try {
                                 float score = core.verify(bestTemplate, bestTemplate);
                                 if (score >= 34.0f) {
@@ -228,7 +253,55 @@ public class OnyxActivity extends Activity {
         finish();
     }
 
-    private byte[]  fileToArrayOfBytes (File file) {
+    private String createImageUriFromFile(File file) {
+        String imageUri = null;
+        byte[] imageBytes = fileToArrayOfBytes(file);
+        if (null != imageBytes) {
+            imageUri = createImageUriFromBytes(imageBytes);
+        }
+        return imageUri;
+    }
+
+    private String createImageUriFromBytes(byte[] imageBytes) {
+        String encodedBytes = null;
+        String imageUri = null;
+        String imageUriPrefix = "data:image/jpeg;base64,";
+        if (null != imageBytes) {
+            encodedBytes = Base64.encodeToString(imageBytes, 0);
+            imageUri = imageUriPrefix + encodedBytes;
+        }
+        return imageUri;
+    }
+
+    private JSONObject getWsqImageJson() throws JSONException {
+        JSONObject wsqImageJson = null;
+        byte[] wsqBytes = null;
+        String encodedBytes = null;
+        int wsqImageNfiqScore = -1;
+        RestoreBitmap restoreBitmap = new RestoreBitmap();
+        Bitmap processedBitmap = restoreBitmap.restoreProcessedBitmapFromFile(
+                mPreprocessedFingerprintImageFile);
+
+        if (null != processedBitmap) {
+            wsqImageJson = new JSONObject();
+            Mat mat = new Mat();
+            Utils.bitmapToMat(processedBitmap, mat);
+            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY); // ensure image is grayscale
+            wsqBytes = core.matToWsq(mat);
+            if (null != wsqBytes) {
+                encodedBytes = Base64.encodeToString(wsqBytes, 0);
+            }
+            NfiqMetrics nfiqMetrics = core.computeNfiq(mat);
+            wsqImageNfiqScore = nfiqMetrics.getNfiqScore();
+        }
+
+        wsqImageJson.put("bytes", encodedBytes);
+        wsqImageJson.put("nfiqScore", wsqImageNfiqScore);
+
+        return wsqImageJson;
+    }
+
+    private byte[] fileToArrayOfBytes(File file) {
         FileInputStream fileInputStream = null;
 
         byte[] bytes = new byte[(int) file.length()];
@@ -238,13 +311,14 @@ public class OnyxActivity extends Activity {
             fileInputStream = new FileInputStream(file);
             fileInputStream.read(bytes);
             fileInputStream.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             String errorMessage = "Error getting file bytes: " + e.toString();
             Log.e(TAG, errorMessage);
         }
         return bytes;
     }
+
     static File mEnrolledFile = null;
 
     private boolean fingerprintExists() {
